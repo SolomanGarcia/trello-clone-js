adee2e2fd7ec546d4d64c284ab2c14c03831b334
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-15 09:58:56 -0700

    add eventListeners to setup function

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -2,6 +2,19 @@ import addGlobalEventListener from "./utils/addGlobalEventListener.js";
 
 export default function setup() {
   addGlobalEventListener("mousedown", "[data-draggable]", (e) => {
-    console.log("mouse down");
+    console.log("down");
+    const mouseMoveFunction = () => {
+      console.log("moved");
+    };
+
+    document.addEventListener("mousemove", mouseMoveFunction);
+    document.addEventListener(
+      "mouseup",
+      () => {
+        document.removeEventListener("mousemove", mouseMoveFunction);
+        console.log("up");
+      },
+      { once: true }
+    );
   });
 }
